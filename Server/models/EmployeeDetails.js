8fb4308b641798e1ea06ac7406b19680c40b0802
Mohmadkh11 <m7md2mh@gmail.com>
2023-05-19 11:56:56 +0300

    11:56

--- Server/models/EmployeeDetails.js
+++ Server/models/EmployeeDetails.js
@@ -2,13 +2,13 @@ const mongoose = require("mongoose");
 const UserDetailsScehma = new mongoose.Schema(
   {
     id: { type: Number, required: true, unique: true },
-    fname: { type: String },
-    lname: { type: String},
+    fname: { type: String , required: true},
+    lname: { type: String , required: true},
     email: { type: String, required: true, unique: true },
-    password: { type: String },
-    address: { type: String },
-    salary: { type: Number },
-    image: { type: String }
+    password: { type: String , required: true},
+    address: { type: String , required: true },
+    salary: { type: Number , required: true },
+    image: { type: String , required: true}
   },
   {
     collection: "EmployeeInfo",
