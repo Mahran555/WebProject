67809a128b8819fab66b3ec6af8deeac9db148e3
Mahran555 <snoker300@gmail.com>
2023-06-19 14:08:22 +0300

    chat fixed

--- frontend/src/Messenger/Chat/ChatBar.jsx
+++ frontend/src/Messenger/Chat/ChatBar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faComments } from '@fortawesome/free-solid-svg-icons';
@@ -8,20 +8,18 @@ import "../../CssFiles/Theme.css"
 
 const ChatBar = ({ SenderId, RecieverId }) => {
     const [error, setError] = useState('');
+    const [inputValue, setInputValue] = useState('');
+    const [response, setResponse] = useState(['string1', 'string2', 'string3']);
+    const chatBoxRef = useRef(null);
 
-    const [Response, setResponse] = useState(['string1', 'string2', 'string3']);
-
-     // Handle sending a message
-
+    // Handle sending a message
     const handleSend = (event) => {
         event.preventDefault();
-        // Get the input element by ID
-        const inputElement = document.getElementById('chat');
 
-        // Read the value from the input element
-        const inputValue = inputElement.value;
-
-      
+        if (inputValue.trim() === '') {
+            // Input value is empty or contains only whitespace
+            return;
+        }
 
         axios.put('http://localhost:5000/Message/' + SenderId + '/' + RecieverId, {
             data: {
@@ -33,6 +31,9 @@ const ChatBar = ({ SenderId, RecieverId }) => {
             .then(res => {
                 if (res.data.status === "Success") {
                     setResponse(res.data.chat.Messages);
+                    setInputValue(''); // Clear the input value
+                    scrollToBottom(); // Scroll to the bottom of the chat after sending a message
+                    
                 }
                 else {
                     setError(res.data.error);
@@ -41,14 +42,13 @@ const ChatBar = ({ SenderId, RecieverId }) => {
             .catch(err => console.log(err));
     }
 
-    // request the messages between both the employees
-    // request to get the Chats that are available between my employee id and another employee
+    // Request the messages between both the employees
     useEffect(() => {
         axios.get('http://localhost:5000/GetMessages/' + SenderId + '/' + RecieverId)
             .then((res) => {
                 if (res.data.status === 'Success') {
                     setResponse(res.data.chat.Messages);
-
+                    scrollToBottom(); // Scroll to the bottom of the chat on initial load
                 }
             })
             .catch(err => {
@@ -57,12 +57,21 @@ const ChatBar = ({ SenderId, RecieverId }) => {
             });
     }, [RecieverId]);
 
+    // Scroll to the bottom of the chat
+    const scrollToBottom = () => {
+        if (chatBoxRef.current) {
+            chatBoxRef.current.scrollTop = chatBoxRef.current.scrollHeight;
+        }
+    };
 
+    useEffect(() => {
+        scrollToBottom(); // Scroll to the bottom when the chat messages change
+    }, [response]);
 
     return (
         <div className="chatBoxWrapper">
-            <div className="chatBoxTop">
-                {Response.map((obj, index) => {
+            <div className="chatBoxTop" ref={chatBoxRef}>
+                {response.map((obj, index) => {
                     return (
                         <div className={`${obj.SenderID == SenderId ? 'message-own' : 'message-other'}`} key={index}>
                             <div className="messageTop">
@@ -72,12 +81,18 @@ const ChatBar = ({ SenderId, RecieverId }) => {
                         </div>
                     );
                 })}
-
             </div>
-            <div className="row w-100 d-flex">
-                <input type="text" className="col-10 input-design ms-3" id="chat" placeholder="chat" />
-                <button className="btn btn-bgc text-white col btn-send" onClick={handleSend}>Send</button>
-            </div>
+            <form className="row w-100 d-flex" onSubmit={handleSend}>
+                <input
+                    type="text"
+                    className="col-10 input-design ms-3 rounded"
+                    id="chat"
+                    placeholder="chat"
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)}
+                />
+                <button className="btn btn-bgc text-white col btn-send" type="submit">Send</button>
+            </form>
         </div>
     );
 }
